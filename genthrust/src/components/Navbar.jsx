ee3954782da4b582189e84d39777d55195dd1f80
Cal <calvin.m71@hotmail.com>
2020-10-04 16:21:00 -0400

    more designs

--- genthrust/src/components/Navbar.jsx
+++ genthrust/src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { Button } from "./Button";
+import "./Navbar.css";
 
 const Navbar = () => {
   const [click, setClick] = useState(false);
@@ -58,7 +60,7 @@ const Navbar = () => {
               </Link>
             </li>
           </ul>
-          {Button && <Button buttonStyle="btn--outline">Sign Up</Button>}
+          {button && <Button buttonStyle="btn--outline">Sign Up</Button>}
         </div>
       </nav>
     </>
