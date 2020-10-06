415c5f03bbc08544474acd803126823d398b52e8
Cal <calvin.m71@hotmail.com>
2020-10-06 19:28:09 -0400

    part-1

--- genthrust/src/pages/About.jsx
+++ genthrust/src/pages/About.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import Footer from "../components/Footer";
+import { motion } from "framer-motion";
+import "../App.css";
 
 const About = () => {
   return (
-    <div>
+    <div className="about-container">
+      <motion.h2 animate={{ fontSize: 50, color: "#104e8b", x: 100, y: -100 }}>
+        About Us
+      </motion.h2>
+
       <Footer />
     </div>
   );
--- genthrust/src/pages/Contact.jsx
+++ genthrust/src/pages/Contact.jsx
@@ -1,35 +1,50 @@
 import React, { useState } from "react";
+
 import Gps from "../components/Gps";
 import Footer from "../components/Footer";
+import "../App.css";
 
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [disabled, setDisabled] = useState(false);
+  const [emailSent, setEmailSent] = useState(null);
+
+  const handleChange = (e) => {
+    setName({ ...name, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventdefault();
+    setDisabled(true);
+  };
+
   return (
     <div className="Contact">
-      {/* <form
-        id="contact-form"
-        onSubmit={this.handleSubmit.bind(this)}
-        method="POST"
-      >
-        <div className="form-group">
-          <label htmlFor="name">Name</label>
-          <input type="text" className="form-control" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="exampleInputEmail1">Email address</label>
-          <input
-            type="email"
-            className="form-control"
-            aria-describedby="emailHelp"
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="message">Message</label>
-          <textarea className="form-control" rows="5"></textarea>
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
-      </form> */}
+      <h1>Contact Us</h1>
+      <form style={{ width: "600px" }} onSubmit={handleSubmit}>
+        <label for="name">Name:</label>
+        <input type="text" name="name" onChange={handleChange} value={name} />
+        <br />
+        <label for="email">Email:</label>
+        <input
+          type="email"
+          name="email"
+          onChange={handleChange}
+          value={email}
+        />
+        <br />
+        <label for="message">Message:</label>
+        <input
+          type="textarea"
+          name="message"
+          onChange={handleChange}
+          value={message}
+        />
+        <br />
+        <button type="submit">Submit</button>
+      </form>
       <Gps />
       <Footer />
     </div>
